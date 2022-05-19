39ded3358134d3f8e70c28f1b9c244d8307325b8
jacob jemal <jacobjemal7@gmail.com>
2022-05-19 10:44:02 -0400

    commit tree df9dd0dc965dae6cf3685dfd9e77039b8b8ae71a

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,8 +7,10 @@ class MoviesController < ApplicationController
   end
 
   def create
-    movie = Movie.create(movie_params)
+    movie = Movie.create!(movie_params)
     render json: movie, status: :created
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { errors: e.record.errors.full_message }, status: :unprocessable_entity
   end
 
   private
